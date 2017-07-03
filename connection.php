e30d03a3b6f037dd5fc40d766c563f0f009d4cc8
Eden Juscelino <eden_juscelino@Eden-Juscelinoui-MacBook-Pro.local>
2017-07-03 23:13:51 +0900

    files created

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+  /**
+   *
+   */
+  class DB
+  {
+    private static $instance = NULL;
+
+    private function __construct() {}
+
+    private function __clone(){}
+
+      public static function
+  }
+
+
+?>

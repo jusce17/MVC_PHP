eee0330ccb27b5dca09b0eff35c223ac0bba1b1f
Eden Juscelino <eden_juscelino@Eden-Juscelinoui-MacBook-Pro.local>
2017-07-04 00:35:30 +0900

    completed

--- connection.php
+++ connection.php
@@ -10,7 +10,15 @@
 
     private function __clone(){}
 
-      public static function
+      public static function getInstance(){
+        if(!isset(self::$instance)){
+          $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+          self::$instance =  new PDO('mysql:host=localhost;dbname=php_mvc', 'root', '', $pdo_options);
+
+        }
+
+        return self::$instance;
+      }
   }
 
 

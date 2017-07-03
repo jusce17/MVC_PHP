741df87e425838f8fcc54689f7f69481b1ede0c5
Eden Juscelino <eden_juscelino@Eden-Juscelinoui-MacBook-Pro.local>
2017-07-04 00:49:18 +0900

    fetching from database

--- routes.php
+++ routes.php
@@ -8,12 +8,17 @@
     switch ($controller) {
       case 'pages':
         $controller = new PagesController();
-        break;
+      break;
+      case 'posts':
+          // we need the model to query the database later in the controller
+          require_once('models/post.php');
+          $controller = new PostsController();
+      break;
 
 
     }
 
-    
+
     // call the action
    $controller->{ $action }();
 
@@ -21,7 +26,8 @@
   // just a list of the controllers we have and their actions
   // we consider those "allowed" values
 
-  $controllers = array('pages' => ['home', 'error']);
+  $controllers = array('pages' => ['home', 'error'],
+                       'posts' => ['index', 'show']);
 
   // check that the requested controller and action are both allowed
   // if someone tries to access something else he will be redirected to the error action of the pages controller
